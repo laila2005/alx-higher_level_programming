e6013ec58abd69c2427d704b5cc3096aa40debc7
laila <laila.mf2005@gmail.com>
2024-09-10 13:14:59 -0700

    task7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,12 +3,13 @@
 
 def islower(c):
     """
-    Checks if the character is a lowercase letter.
-
-    Args:
-    c (str): The character to check.
-
-    Returns:
-    bool: True if c is a lowercase letter, False otherwise.
+    Returns True if c is a lowercase character, False otherwise.
     """
-    return 'a' <= c <= 'z'
+    # Use the ord() function to get the ASCII value of the character
+    ascii_val = ord(c)
+    
+    # Check if the ASCII value falls within the range of lowercase letters
+    if 97 <= ascii_val <= 122:
+        return True
+    else:
+        return False
