e97a0109eaf230b9afcdbc4c2af514bb091afa2a
laila <laila.mf2005@gmail.com>
2024-09-10 05:58:44 -0700

    task7

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def islower(c):
+    """
+    Checks if the character is a lowercase letter.
+
+    Args:
+    c (str): The character to check.
+
+    Returns:
+    bool: True if c is a lowercase letter, False otherwise.
+    """
+    return 'a' <= c <= 'z'
