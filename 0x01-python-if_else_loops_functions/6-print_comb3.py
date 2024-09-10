9ee60a94a05d0de24e231fb3e836e754d0d48088
laila <laila.mf2005@gmail.com>
2024-09-10 05:39:02 -0700

    task6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# Print all possible different combinations of two digits
+for i in range(10):
+    for j in range(i + 1, 10):
+        # Print combination with formatting
+        print(f"{i}{j}", end=", " if i != 8 or j != 9 else "\n")
