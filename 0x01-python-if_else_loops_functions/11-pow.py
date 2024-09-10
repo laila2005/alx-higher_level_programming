4a2ac0e363068165001076efe991d72135b38973
laila <laila.mf2005@gmail.com>
2024-09-10 09:09:48 -0700

    task10

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+     """
+    Computes a to the power of b and returns the result.
+
+    Args:
+        a (int): The base number.
+        b (int): The exponent.
+
+    Returns:
+        int: The result of a raised to the power of b.
+    """
+     result = 1
+    for _ in range(b):
+        result *= a
+    return result
