9cc937ace175ab15dc008a94c5005578118f4406
laila <laila.mf2005@gmail.com>
2024-09-12 06:23:12 -0700

    task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+
+num_args = len(sys.argv) -1
+
+if num_args == 1:
+    print("{} argument.".format(num_args))
+else:
+    print("{} arguments.".format(num_args))
+
+for i in range(1, len(sys.argv)):
+    print("{}: {}".format(i, sys.argv[i]))
