8d2b95b377e7dceee3317a5451221f63bc4ba5a3
laila <laila.mf2005@gmail.com>
2024-09-12 07:02:00 -0700

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,15 +3,15 @@
 def main():
     num_args = len(sys.argv) - 1
 
-if num_args == 1:
-    print("{} argument:".format(num_args))
-elif num_args == 0:
-    print("{} arguments.".format(num_args))
-else:
-    print("{} arguments:".format(num_args))
+    if num_args == 1:
+       print("{} argument:".format(num_args))
+    elif num_args == 0:
+       print("{} arguments.".format(num_args))
+    else:
+       print("{} arguments:".format(num_args))
 
-for i in range(1, len(sys.argv)):
-    print("{}: {}".format(i, sys.argv[i]))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
 
 if __name__ == "__main__":
     main()
