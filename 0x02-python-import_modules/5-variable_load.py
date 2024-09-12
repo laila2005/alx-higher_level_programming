a7a9e87551c206cb4298f3439dcf752442f00761
laila <laila.mf2005@gmail.com>
2024-09-12 10:57:11 -0700

    task5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 from variable_load_5 import a
-
 def main():
     print("{}".format(a))
-
-# The if block should be outside of the main() function
 if __name__ == "__main__":
     main()
