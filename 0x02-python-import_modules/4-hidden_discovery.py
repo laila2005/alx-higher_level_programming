dab97153e2556f16502cd3b5fe45c4fdf2d31c45
laila <laila.mf2005@gmail.com>
2024-09-12 10:44:03 -0700

    task3

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import hidden_4
+import sys
+
+if __name__ == "__main__":
+    # Get all the names defined in the module
+    names = dir(hidden_4)
+    
+    # Filter out names that start with __
+    filtered_names = [name for name in names if not name.startswith("__")]
+    
+    # Sort names alphabetically
+    filtered_names.sort()
+
+    # Print each name on a new line
+    for name in filtered_names:
+        print(name)
