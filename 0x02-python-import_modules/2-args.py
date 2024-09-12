2dd07f4d80e784fd7ae781af6d0dddb6fd565127
laila <laila.mf2005@gmail.com>
2024-09-12 06:29:04 -0700

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ num_args = len(sys.argv) -1
 if num_args == 1:
     print("{} argument:".format(num_args))
 elif num_args == 0:
-    print("{} argument.".format(num_args))
+    print("{} arguments.".format(num_args))
 else:
     print("{} arguments:".format(num_args))
 
