ac35d81d69348b740090de09b863db290c8133b3
laila <laila.mf2005@gmail.com>
2024-09-15 06:53:58 -0700

    task5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    char_removed = ['c', 'C']
-    filtered = (char for char in my_string if char not in char_removed)
-    modified_string = ''.join(filtered)
-    return modified_string
+    new_string = []
+    for o in my_string:
+        if o != 'c' and o != 'C':
+            new_string.append(o)
+    return ''.join(new_string)
