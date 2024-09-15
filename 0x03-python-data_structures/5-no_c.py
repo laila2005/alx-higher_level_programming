ee23571b66e60f1d83336926fb3650a956898b42
laila <laila.mf2005@gmail.com>
2024-09-15 07:01:49 -0700

    task5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,16 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    """
-    Removes all occurrences of 'c' and 'C'
-    from the input string and returns the result.
-    Parameters:
-    my_string (str): The string from which
-    characters 'c' and 'C' will be removed.
-    Returns:
-    str: The modified string with 'c' and 'C' removed.
-    """
-    result = []
+    new_string = ""
     for char in my_string:
-        if char != 'c' and char != 'C':
-            result.append(char)
-    return ''.join(result)
+        if char.lower() != 'c':
+            new_string += char
+    return new_string
