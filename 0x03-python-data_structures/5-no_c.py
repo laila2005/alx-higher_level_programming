fdcd4012d5fee9629013bf5401999ab1a3ccfa4c
laila <laila.mf2005@gmail.com>
2024-09-15 07:06:11 -0700

    task5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = []
-    for o in my_string:
-        if o != 'c' and o != 'C':
-            new_string.append(o)
-    return ''.join(new_string)
+    new_string = ""
+    for char in my_string:
+        if char.lower() != 'c':
+            new_string += char
+    return new_string
