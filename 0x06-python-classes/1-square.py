95f61dbb35f5d495a047e84e6e04c763f9fb88fe
laila <laila.mf2005@gmail.com>
2024-10-04 02:58:38 -0700

    task1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 """
 This module defines a class Square.
-It defines a square by a private instance attribute size.
 """
 
 
 class Square:
-    """
-    A class that defines a square by a private instance attribute: size.
-    The size attribute is initialized during instantiation.
-    """
+    """Represent a square."""
 
+    def __init__(self, size=0):
+        """Initialize a new Square.
 
-def __init__(self, size):
-    """
-        Initialize a new Square instance with a private attribute size.
         Args:
-            size: The size of the square.
+            size (int): The size of the new square.
         """
-    self.__size = size
+        self.__size = size
