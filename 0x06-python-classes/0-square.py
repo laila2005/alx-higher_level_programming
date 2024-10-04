2690fee1968798f795884b9079242e48aaca1ec9
laila <laila.mf2005@gmail.com>
2024-10-04 02:16:26 -0700

    pycodestyle friendly

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-python3 -c 'print(__import__("my_module").MyClass.__doc__)'
-class Square():
-    """ empty class Square """
+"""
+This module defines an empty class Square.
+It is intended to define a square for future use.
+"""
+
+
+class Square:
+    """ An empty class that defines a square. """
     pass
