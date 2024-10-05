206cabb5e619fad7c33c2cda185e6bc8bbf34d64
laila <laila.mf2005@gmail.com>
2024-10-05 14:11:03 -0700

    task0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,40 @@
 #!/usr/bin/python3
+import unittest
+
 def add_integer(a, b=98):
+    """ adding two integrs a , b.
+
+    Args:
+        a:the 1st number should be int
+        b:the 2nd number by defult = 98, should be int
+    returns:
+        the sum of two numbers
+    raises:
+        TypeError: If a or b is not an integer or a float.
+
+    Examples:
+        >>> add_integer(1, 2)
+        3
+        >>> add_integer(100, -2)
+        98
+        >>> add_integer(2.5, 3)
+        5
+        >>> add_integer(10)
+        108
+        >>> add_integer("a", 2)
+        Traceback (most recent call last):
+        TypeError: a must be an integer
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
     a = int(a)
     b = int(b)
+
     return a + b
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
