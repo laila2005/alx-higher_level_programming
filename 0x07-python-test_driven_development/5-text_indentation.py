c8a0628b9f667d55f5d41efdc70e0c9f070fe639
laila <laila.mf2005@gmail.com>
2024-10-09 09:55:39 -0700

    asdjustments for task4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""
+this module Prints a text with 2 new lines after each char
+"""
 def text_indentation(text):
     """
     Prints a text with 2 new lines after each of these characters: ., ?, and :
