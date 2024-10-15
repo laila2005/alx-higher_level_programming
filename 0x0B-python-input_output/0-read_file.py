8e0d444bde8f5a4b555bc1ba9f9d02c3b8c81b95
laila <laila.mf2005@gmail.com>
2024-10-15 11:31:01 -0700

    task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+This module provides a function to read a
+UTF-8 encoded text file and print its content to stdout.
+"""
+
+
 def read_file(filename=""):
-    """Reads a UTF-8 text file and prints its content to stdout."""
+    """
+    Reads a UTF-8 text file and prints its content to stdout.
+
+    Args:
+        filename (str): The name of the
+        file to read. Defaults to an empty string.
+    """
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read(), end="")
