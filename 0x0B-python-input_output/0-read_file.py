629db7801ac26fa9164fb29bb6dd62ac532f759b
laila <laila.mf2005@gmail.com>
2024-10-15 11:26:18 -0700

    task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,4 @@
-#!/usr/bin/python
-"""
-this module reads a text file (UTF8) and prints it to stdout
-"""
-
-
+#!/usr/bin/python3
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its content to stdout."""
     with open(filename, "r", encoding="utf-8") as file:
