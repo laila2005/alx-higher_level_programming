4d774f51b381d312afe8653adf407ae69f2c6fbb
laila <laila.mf2005@gmail.com>
2024-10-15 11:42:36 -0700

    task4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module provides a function
+that returns a Python object
+from a JSON string representation.
+"""
+
+def from_json_string(my_str):
+    """
+    Returns a Python object represented by a JSON string.
+
+    Args:
+        my_str (str): The JSON string to convert.
+
+    Returns:
+        any: The Python object represented by the JSON string.
+    """
+    return json.loads(my_str)
