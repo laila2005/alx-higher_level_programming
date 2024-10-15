da1ca878e1e813c32de37b0d1949d0709ff9f4c9
laila <laila.mf2005@gmail.com>
2024-10-15 11:41:03 -0700

    task3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module provides a function
+that returns the JSON representation of an object.
+"""
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string).
+
+    Args:
+        my_obj (any): The object to convert to a JSON string.
+
+    Returns:
+        str: The JSON representation of the object.
+    """
+    return json.dumps(my_obj)
