d620e255c13a744f3824c5de26e993c9c0bd2a3a
laila <laila.mf2005@gmail.com>
2024-10-16 23:43:39 -0700

    pycodestyle changes

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -8,7 +8,6 @@ class Rectangle:
     """
     Class that defines a rectangle with width and height.
     """
-    
     def __init__(self, width=0, height=0):
         """
         Initializes the rectangle with optional width and height.
