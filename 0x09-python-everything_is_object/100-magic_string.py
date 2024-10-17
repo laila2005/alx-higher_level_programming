548939ba9879ac261acb8759ee699bf4ba7d3199
laila <laila.mf2005@gmail.com>
2024-10-16 23:29:08 -0700

    restored

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def magic_string():
+    """
+    function 
+    """
     magic_string.counter = getattr(magic_string, 'counter', 0) + 1
-    return ("BestSchool, " * (magic_string.counter - 1) + "BestSchool")
+    return ("BestSchool, " * (magic_string.counter - 1) + "BestSchooli")
