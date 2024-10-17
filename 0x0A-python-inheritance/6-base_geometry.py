395d31c0c00fac3e48953899cc98cd113e047d56
laila <laila.mf2005@gmail.com>
2024-10-17 05:16:05 -0700

    task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+this module includes a function that 
+its a Public instance method
+"""
+
+
+class BaseGeometry:
+    """
+    A class that serves as a base for geometrical shapes.
+    """
+    def area(self):
+        """
+        Calculates the area of the geometry.
+        Raises an Exception if the method is not implemented.
+        """
+        raise Exception("area() is not implemented")
