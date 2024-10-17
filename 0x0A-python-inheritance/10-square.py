3d60eefd5516e6d8c9004369f6b3d82f3bb9695e
laila <laila.mf2005@gmail.com>
2024-10-17 06:37:24 -0700

    task10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+this module represents a class Square
+that inherits from Rectangle
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class with an area method
+    and integer validator
+    """
+
+    def area(self):
+        """Raises an Exception."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates the integer value."""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """A class that defines a rectangle based on BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Initialize a new Rectangle instance."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Calculates the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a string representation of the rectangle."""
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+class Square(Rectangle):
+    """A class that defines a square based on Rectangle."""
+
+    def __init__(self, size):
+        """Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Calculates the area of the square."""
+        return self.__size * self.__size
