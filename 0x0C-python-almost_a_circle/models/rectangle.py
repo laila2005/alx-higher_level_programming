8c1dd4812393d3ba1cf9b0750df1d3b26b483b91
laila <laila.mf2005@gmail.com>
2024-10-19 05:47:51 -0700

    rectangle class has been created

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Module models.rectangle
+This module contains a class Rectangle that inherits from Base
+"""
+
+from models.base import Base
+
+class Rectangle(Base):
+    """
+    Rectangle class that inherits from Base.
+    Handles width, height, x, and y attributes with validation.
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
