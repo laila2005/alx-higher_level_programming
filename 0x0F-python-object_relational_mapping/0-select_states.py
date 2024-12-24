d72c79e034f7bef3400b34a80db9f692bf1ed7f8
laila <laila.mf2005@gmail.com>
2024-12-24 05:47:48 -0800

    task1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == "__main__":
 
-db = MySQLdb.connect(
+    db = MySQLdb.connect(
     host="localhost",
     port=3306,
     user=sys.argv[1],
@@ -16,7 +16,7 @@ db = MySQLdb.connect(
 )
 
 cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
+cur.execute("SELECT DISTINCT id, name FROM states ORDER BY id ASC")
 rows = cur.fetchall()
 for row in rows:
     print(row)
