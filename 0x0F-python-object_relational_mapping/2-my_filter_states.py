b9514b5fa2d656e60f22a073d471a24867d196d0
laila <laila.mf2005@gmail.com>
2024-12-25 10:49:18 -0800

    task2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module that lists all states from the database hbtn_0e_0_usa
+that start with 'N'.
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    query = (
+        "SELECT id, name FROM states "
+        "WHERE name = BINARY %s ORDER BY id ASC"
+    )
+    cur.execute(query, (sys.argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
