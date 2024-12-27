93671a1ba6b24f7d234fd031d524bc848e7dacc6
laila <laila.mf2005@gmail.com>
2024-12-27 06:12:00 -0800

    task6

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,7 +6,6 @@ where the state name matches the argument.
 import MySQLdb
 import sys
 if __name__ == "__main__":
-    # Connect to MySQL database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -14,9 +13,7 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
-    # Create cursor
     cur = db.cursor()
-    # Use parameterized query to avoid SQL injection
     query = """
     SELECT cities.name
     FROM cities
@@ -24,15 +21,11 @@ if __name__ == "__main__":
     WHERE states.name = %s
     ORDER BY cities.id ASC;
     """
-    # Execute query with the state name argument
     cur.execute(query, (sys.argv[4],))
-    # Fetch all rows
     rows = cur.fetchall()
-    # Print cities or a message if no cities are found
     if rows:
         print(", ".join([row[0] for row in rows]))
     else:
         print(" ")
-    # Close the cursor and the database connection
-    cur.close()
+   cur.close()
     db.close()
